281239eccefe3804783b133650ff4c342dbaa86f
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-04-14 02:10:23 -0300

    usando state

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,12 +1,12 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Formulario.css";
 function Formulario() {
 
+const [nome, setNome] = useState("");
+const [email, setEmail] = useState("");
+const [mensagem, setMensagem] = useState("");
 
 const enviarMensagem = () => {
-const nome = document.querySelector('.nome').value;
-const email = document.querySelector('.email').value;
-const mensagem = document.querySelector('.mensagem').value;
 const texto = `Nome: ${nome}\nE-mail: ${email}\nMensagem: ${mensagem}`;
 const textoCodificado = encodeURIComponent(texto);
 const numeroWhatsApp = '5581998515887';
@@ -24,9 +24,9 @@ const handleSubmit = (event) => {
     <div className="formSide">
       <h1>Envie-me uma mensagem</h1>
       <form onSubmit={handleSubmit}>
-        <input required className="nome" type="name" placeholder="Nome"/>
-        <input required className="email" type="email" placeholder="E-mail"/>
-        <input required className="mensagem"type="text" placeholder="Mensagem"/>
+        <input required className="nome" type="name" placeholder="Nome" onChange={(event) => setNome(event.target.value)}/>
+        <input required className="email" type="email" placeholder="E-mail" onChange={(event) => setEmail(event.target.value)}/>
+        <input required className="mensagem" type="text" placeholder="Mensagem" onChange={(event) => setMensagem(event.target.value)}/>
         <button className="botaoEnviar" type="submit">Enviar</button>
       </form>
       </div>
