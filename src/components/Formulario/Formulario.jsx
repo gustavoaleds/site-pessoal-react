3b26192fb546d01badf62aade7b5fdc3c49281fa
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-04-14 01:16:26 -0300

    aplicando função para o whatsapp

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,18 +1,40 @@
 import React from "react";
 import "./Formulario.css";
-
 function Formulario() {
+
+
+const enviarMensagem = () => {
+  console.log('A função foi chamada!')
+
+
+const nome = document.querySelector('.nome').value;
+const email = document.querySelector('.email').value;
+const mensagem = document.querySelector('.mensagem').value;
+const texto = `Nome: ${nome}\nE-mail: ${email}\nMensagem: ${mensagem}`;
+const textoCodificado = encodeURIComponent(texto);
+const numeroWhatsApp = '5581998515887';
+const url = `https://wa.me/${numeroWhatsApp}?text=${textoCodificado}`;
+
+window.open(url, '_blank');
+};
+
+const handleSubmit = (event) => {
+  event.preventDefault();
+  enviarMensagem();
+};
+
   return (
     <div className="formSide">
       <h1>Envie-me uma mensagem</h1>
-      <form>
-        <input className="nome" type="name" placeholder="Nome"/>
-        <input className="email" type="email" placeholder="E-mail"/>
-        <input className="mensagem"type="text" placeholder="Mensagem"/>
-        <input className="botaoEnviar" type="submit"/>
+      <form onSubmit={handleSubmit}>
+        <input required className="nome" type="name" placeholder="Nome"/>
+        <input required className="email" type="email" placeholder="E-mail"/>
+        <input required className="mensagem"type="text" placeholder="Mensagem"/>
+        <button className="botaoEnviar" type="submit">Enviar</button>
       </form>
-    </div>
-  )}
+      </div>
+    )
+    }
 
 
 export default Formulario;
