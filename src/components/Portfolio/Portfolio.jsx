471be1d35de2f1292ebc69724c3a6cd503ff20b6
Gustavo Alexandre <gustavoslyth1@gmail.com>
2023-04-13 23:25:29 -0300

    ajuste de bug visual

--- src/components/Portfolio/Portfolio.jsx
+++ src/components/Portfolio/Portfolio.jsx
@@ -1,32 +1,21 @@
 import React from "react";
 import "./Portfolio.css";
 import Dados from "./Portfolio.json"
+
 function Portfolio(){
   return(
+
     <div className="mainContentPortfolio">
     <h1>Portfolio</h1>
     <div className="galeria">
 
-      <div className="item-galeria">
-           <h2>{Dados.titulo[0]}</h2>
-           <p>{Dados.resumo[0]}</p>
-           <button>Acessar Demo</button>
-      </div>
-
-      <div className="item-galeria">
-           <h2>{Dados.titulo[1]}</h2>
-           <p>{Dados.resumo[1]}</p>
-           <button>Acessar Demo</button>
-      </div>
-
-      <div className="item-galeria">
-           <h2>{Dados.titulo[2]}</h2>
-           <p>{Dados.resumo[2]}</p>
-           <button>Acessar Demo</button>
-      </div>
-
-      </div>
-      </div>
-      )
-}
+      {Dados.itemDaGaleria.map((item, index) => (
+        <div key={index} className="item-galeria">
+          <h2>{item.titulo}</h2>
+          <p>{item.resumo}</p>
+          <button><a href={item.link}>Acessar Demo</a></button>
+        </div>))}
+        </div>
+        </div>
+      )}
 export default Portfolio;
